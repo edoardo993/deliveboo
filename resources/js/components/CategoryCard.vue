af5d1f4ae2e2cb92220b426ae1a90bdf6ee612d1
Edoardo Piragine <edoardo.piragine@gmail.com>
2021-04-19 14:42:23 +0200

    updates

---
+++ resources/js/components/CategoryCard.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <div>
+
+        <div class="single-card-category">
+
+            <!--
+                da implementare tramite chiamata API a tabella img
+                fatta in DB (come sfondo o come img normale)
+            -->
+            <img>
+
+            <h3 class="category-name">{{ categories.name }}</h3>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'category-card',
+        props: {
+            categories: Object
+        }
+    }
+</script>
