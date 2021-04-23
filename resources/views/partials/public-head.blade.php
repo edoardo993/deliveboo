09db863d94b0932169f6d057e63865e590ed8439
Lorenzo-Arnau <skyline03@hotmail.it>
2021-04-23 19:47:25 +0200

    responsive update tablet

--- resources/views/partials/public-head.blade.php
+++ resources/views/partials/public-head.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HOMEPAGE</title>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Inter&family=Acme:wght@400&display=swap" rel="stylesheet">
